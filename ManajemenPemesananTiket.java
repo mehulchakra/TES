0ce62a31226532d1b27209d374453f9981dfe27b
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 14:44:46 +0700

    Create ManajemenPemesananTiket.java

---
+++ ManajemenPemesananTiket.java
@@ -0,0 +1,11 @@
+public class ManajemenPemesananTiket {
+    private AgenTiket agenTiket;
+
+    public ManajemenPemesananTiket(AgenTiket agenTiket) {
+        this.agenTiket = agenTiket;
+    }
+
+    public AgenTiket getAgenTiket() {
+        return agenTiket;
+    }
+}

7fa6184f716bb92bdfbb7ca4fb1042fe154a6bb9
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 15:03:48 +0700

    Update PemesananTiket.java

--- PemesananTiket.java
+++ PemesananTiket.java
@@ -1,3 +1,4 @@
+// Interface PemesananTiket dengan metode untuk pemesanan dan tampilan daftar pemesanan
 public interface PemesananTiket {
     void pesanTiket(String namaKonser, int jumlahTiket);
     void tampilkanDaftarPemesanan();

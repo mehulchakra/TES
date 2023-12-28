ac239a66e14b4f18100ebf78836f0295c00ba8f2
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 15:04:47 +0700

    Update AgenTiket.java

--- AgenTiket.java
+++ AgenTiket.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 
+// Kelas AgenTiket mengimplementasikan interface PemesananTiket
 public class AgenTiket implements PemesananTiket {
+        // Daftar konser dan pemesanan yang tersedia
     public ArrayList<Konser> daftarKonser = new ArrayList<>();
     private ArrayList<String> daftarPemesanan = new ArrayList<>();
-
+    
+    // Implementasi metode dari interface PemesananTiket
     @Override
     public void pesanTiket(String namaKonser, int jumlahTiket) {
         daftarPemesanan.add("Pemesanan: " + namaKonser + ", Jumlah: " + jumlahTiket);
@@ -15,11 +18,13 @@ public class AgenTiket implements PemesananTiket {
             System.out.println(pemesanan);
         }
     }
-
+    
+    // Metode untuk menambahkan konser ke daftar
     public void tambahKonser(Konser konser) {
         daftarKonser.add(konser);
     }
-
+    
+    // Metode untuk menampilkan daftar konser
     public void tampilkanDaftarKonser() {
         for (Konser konser : daftarKonser) {
             System.out.println(konser.getInfo());

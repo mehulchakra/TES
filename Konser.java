dd5c96ceda7d3681df88b328d4d7da52c429065a
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 15:03:32 +0700

    Update Konser.java

--- Konser.java
+++ Konser.java
@@ -1,14 +1,18 @@
+// Kelas Konser menyimpan informasi dasar tentang konser
 public class Konser {
+        // Atribut-atribut kelas Konser
     private String namaKonser;
     private String tanggal;
     private double hargaTiket;
-
+    
+    // Konstruktor kelas Konser
     public Konser(String namaKonser, String tanggal, double hargaTiket) {
         this.namaKonser = namaKonser;
         this.tanggal = tanggal;
         this.hargaTiket = hargaTiket;
     }
-
+    
+    // Method getInfo() untuk mendapatkan informasi konser
     public String getInfo() {
         return "Konser: " + namaKonser + ", Tanggal: " + tanggal + ", Harga Tiket: " + hargaTiket;
     }

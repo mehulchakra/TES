e5ec1ec0f5affd92d2e276f524ce6b49081089b2
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 15:05:21 +0700

    Update ManajemenPemesananTiket.java

--- ManajemenPemesananTiket.java
+++ ManajemenPemesananTiket.java
@@ -1,10 +1,13 @@
+// Kelas ManajemenPemesananTiket untuk mengelola pemesanan tiket
 public class ManajemenPemesananTiket {
+        // Atribut agenTiket untuk menyimpan informasi agen tiket
     private AgenTiket agenTiket;
-
+    
+    // Konstruktor kelas ManajemenPemesananTiket
     public ManajemenPemesananTiket(AgenTiket agenTiket) {
         this.agenTiket = agenTiket;
     }
-
+    // Getter untuk agenTiket
     public AgenTiket getAgenTiket() {
         return agenTiket;
     }
